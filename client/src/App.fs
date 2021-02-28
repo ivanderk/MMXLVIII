6034ee32eb24f483fff508022125085ff1210014
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2021-02-28 22:01:48 +0100

    Added Feliz client

---
+++ client/src/App.fs
@@ -0,0 +1,22 @@
+module App
+open Feliz
+
+[<ReactComponent>]
+let Counter() =
+    let (count, setCount) = React.useState(0)
+    Html.div [
+        Html.button [
+            prop.style [ style.marginRight 5 ]
+            prop.onClick (fun _ -> setCount(count + 1))
+            prop.text "Increment"
+        ]
+
+        Html.button [
+            prop.style [ style.marginLeft 5 ]
+            prop.onClick (fun _ -> setCount(count - 1))
+            prop.text "Decrement"
+        ]
+
+        Html.h1 count
+    ]
+
